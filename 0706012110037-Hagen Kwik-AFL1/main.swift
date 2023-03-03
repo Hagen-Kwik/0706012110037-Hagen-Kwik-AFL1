c38723c1babb9dff180298db5499c33057c5a972
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-03-03 10:32:59 +0700

    Progress

--- 0706012110037-Hagen Kwik-AFL1/main.swift
+++ 0706012110037-Hagen Kwik-AFL1/main.swift
@@ -7,5 +7,103 @@
 
 import Foundation
 
-print("Hello, World!")
+let tukutuku: KeyValuePairs<String, Int> = ["Rawon": 20000, "Sate": 15000, "Nasi Goreng": 10000, "Jus Jeruk": 5000]
+let gotri: KeyValuePairs<String, Int>  = ["Tahu Telor": 17000, "Soto": 12000, "Nasi Bakar": 10000, "Air Mineral": 2000]
+let madamlie: KeyValuePairs<String, Int>  = ["Dadar Jagung": 8000, "Kikil": 30000, "Lemper": 10000, "Kopi Tubruk": 7000]
+let kopte: KeyValuePairs<String, Int>  = ["Tempe": 6000, "Nasi Padang": 50000, "Nasi Limau": 100000, "Jahe": 12000]
 
+
+
+var shops : Array<KeyValuePairs<String, Int>> = Array()
+shops.append(tukutuku)
+shops.append(gotri)
+shops.append(madamlie)
+shops.append(kopte)
+
+var shopnames: Array<String> = ["Tuku-Tuku","Gotri", "Madam Lie", "Kopte"]
+
+var cart : Array<KeyValuePairs<String, Int>> = Array()
+
+var userInput:String = " "
+
+repeat{
+    print("Welcome to UC Walk Cafeteria 🧑‍🍳👩‍🍳")
+    print("""
+        Please choose a cafeteria:
+        [1] Tuku-Tuku
+        [2] Gotri
+        [3] Madam Lie
+        [4] Kopte
+         -
+        [S]hopping Cart
+        [Q]uit
+        Your cafeteria choice?
+    """)
+    
+    //Logic for choosing
+    while(true){
+        userInput = readLine()?.uppercased() ?? " "
+        //for shopping cart or quit
+        if userInput == "S" || userInput == "Q" {
+            break
+        }
+        //check if number
+        if Int(userInput) ?? -1 > 0 && Int(userInput) ?? -1 > 0{
+            //user input -1 because index start from 0
+            userInput = String(Int(userInput)!-1)
+            break
+        }
+        print("That is not a valid response! Please Try Again.")
+    }
+    
+    switch(userInput){
+        //quit
+    case "Q":
+        break
+        //shopping cart
+    case "S":
+        var total_price = 0
+        
+        if cart.count == 0  {
+            print("")
+            print("Your Cart is empty")
+            print("")
+        } else {
+            //print cart
+            print("""
+            Press [B] to go back
+            Press [P] to pay / checkout
+            Your choice?
+            """)
+            
+            //calculate price
+            for itemsInCart in cart {
+                var tempSHOP = shops[itemsInCart["resNameIndex"]]
+                total_price = tempSHOP[itemsInCart["food"]].value * itemsInCart["totalItems"]
+            }
+            
+            var userPay = " "
+               
+            
+            while(true){
+                userPay = readLine()?.uppercased() ?? " "
+                //for shopping cart or quit
+                if userPay == "B" || userPay == "P" {
+                    break
+                }
+                print("That is not a valid response! Please Try Again.")
+            }
+                
+            if(userPay == "B"){
+                break
+            } else{
+                pay(total_price: total_price)
+                break
+            }
+        }
+            	
+    default:
+        printFoods(Menu: shops[Int(userInput)!], shopNameIndex: Int(userInput)!, shopName: shopnames[Int(userInput)!])
+    }
+    
+} while(userInput != "Q")
---
+++ 0706012110037-Hagen Kwik-AFL1/restaurant.swift
@@ -0,0 +1,112 @@
+//
+//  restaurant.swift
+//  0706012110037-Hagen Kwik-AFL1
+//
+//  Created by MacBook Pro on 01/03/23.
+//
+
+import Foundation
+
+
+func printFoods(Menu: KeyValuePairs<String, Int>, shopNameIndex: Int, shopName: String){
+    var userInputForFood = " "
+    
+    print("Hi, welcome back to \(shopName)!")
+    print("What would you like to order?")
+    
+    while true{
+        var num = 1
+        for MenuItems in Menu{
+            print("[\(num)] \(MenuItems.key)")
+            num += 1
+        }
+        print("""
+    -
+    [B]ack to main Menu?
+    Your menu choice?
+    """)
+        
+    //CHOOSINF CHECKER LOGIC
+    while true{
+        userInputForFood = readLine()?.uppercased() ?? " "
+        //for shopping cart or quit
+        if userInputForFood == "B" {
+            break
+        }
+        //check if number
+        if Int(userInputForFood) ?? -1 < Menu.count+1 && Int(userInputForFood) ?? -1 > 0{
+            userInputForFood = String(Int(userInputForFood)!-1)
+            break
+        }
+        
+        print("That is not a valid response! Please Try Again.")
+    }
+    
+    switch(userInputForFood){
+        case "B":
+            break
+        default:
+            print(Menu[Int(userInputForFood)!].key + "@ \(Menu[Int(userInputForFood)!].value)")
+        
+            print("How many \(Menu[Int(userInputForFood)!].key) do you want to buy ?")
+            
+            var userInputForBuying = " "
+
+            while true {
+                userInputForBuying = readLine()?.uppercased() ?? " "
+                if Int(userInputForBuying) ?? -1 > 0{
+                    break
+                }
+                print("Please Enter a valid number")
+            }
+        
+            addFoodtoCart(resNameIndex: shopNameIndex, food: Int(userInputForFood)!, totalItems: Int(userInputForBuying)!)
+
+            print("Thank You for Ordering")
+        }
+    
+    break
+    }
+}
+
+func addFoodtoCart(resNameIndex: Int, food: Int, totalItems: Int) {
+    var temp: KeyValuePairs<String, Int> = ["resNameIndex": resNameIndex, "food": food, "totalItems": totalItems]
+    cart.append(temp)
+}
+
+
+func pay(total_price: Int){
+    print("Your total order: \(total_price)")
+    print("Enter the amount of your money: ")
+    
+    var your_money = " "
+    
+    while true {
+        your_money = readLine() ?? " "
+        //bigger than 0 and is bigger than pice
+        if Int(your_money) ?? -1 > 0 && Int(your_money) ?? -1 >= total_price{
+            break
+        } else {
+            print("Payment not enough")
+        }
+        
+        if Int(your_money) ?? -1 == -1 || Int(your_money) ?? -1 == 0{
+            print("Please enter a valid amount")
+        }
+    }
+    
+    print("You pay \(your_money)  Change:\(Int(your_money)! - total_price)")
+    print("Enjoy your meals!")
+    
+    cart.removeAll()
+    
+    var returnBool = "A"
+
+    while true{
+        print("Press [return] to go back to main screen")
+        returnBool = readLine() ?? "A"
+        if returnBool == "" {
+            break
+        }
+    }
+}
