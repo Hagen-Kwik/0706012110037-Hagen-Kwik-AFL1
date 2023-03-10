96885d1a84436466552ef7e219e25d041fe8692f
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-03-10 09:08:15 +0700

    DONE FINAL

--- 0706012110037-Hagen Kwik-AFL1/main.swift
+++ 0706012110037-Hagen Kwik-AFL1/main.swift
@@ -7,21 +7,25 @@
 
 import Foundation
 
+//  Initiate Food Items/menu as Key value pairs
 let tukutuku: KeyValuePairs<String, Int> = ["Rawon": 20000, "Sate": 15000, "Nasi Goreng": 10000, "Jus Jeruk": 5000]
 let gotri: KeyValuePairs<String, Int>  = ["Tahu Telor": 17000, "Soto": 12000, "Nasi Bakar": 10000, "Air Mineral": 2000]
 let madamlie: KeyValuePairs<String, Int>  = ["Dadar Jagung": 8000, "Kikil": 30000, "Lemper": 10000, "Kopi Tubruk": 7000]
 let kopte: KeyValuePairs<String, Int>  = ["Tempe": 6000, "Nasi Padang": 50000, "Nasi Limau": 100000, "Jahe": 12000]
 
 
-
+// put menu of each restaurant using another key value pair
 var shops : KeyValuePairs<String, KeyValuePairs<String, Int> > = ["Tuku-Tuku": tukutuku, "Gotri": gotri, "Madam Lie": madamlie, "Kopte": kopte]
 
+//initiate shopping  cart
 var carts : Dictionary<String, Array< Dictionary<String, Any> > > = [:]
 
-
+//initiate fur user input
 var userInput:String = " "
 
+//give do while
 repeat{
+    // print starting point
     print("Welcome to UC Walk Cafeteria 🧑‍🍳👩‍🍳")
     print("""
     Please choose a cafeteria:
@@ -37,12 +41,13 @@ repeat{
     
     //Logic for choosing
     while(true){
+        //input is uppercased
         userInput = readLine()?.uppercased() ?? " "
         //for shopping cart or quit
         if userInput == "S" || userInput == "Q" {
             break
         }
-        //check if number
+        //check if number and its limimt constraints
         if Int(userInput) ?? -1 > 0 && Int(userInput) ?? -1 < shops.count + 1{
             //user input -1 because index start from 0
             userInput = String(Int(userInput)!-1)
@@ -50,16 +55,19 @@ repeat{
         }
         print("That is not a valid response! Please Try Again.")
     }
+    //logic for choosing end
     
+    //switch case for user input
     switch(userInput){
         //quit
     case "Q":
         break
         //shopping cart
-    case "S":   
+    case "S":
+        //initiate total price for counting later
         var total_price = 0
         
-        
+        //check if cart is empty
         if carts.isEmpty {
             print("")
             print("Your Cart is empty")
@@ -71,8 +79,8 @@ repeat{
                 print("Your order from \(itemsInCart.key): ")
     
                 for itemsInsideEachShop in itemsInCart.value {
-                    var price: Int = itemsInsideEachShop["Price"] as! Int
-                    var qty: Int  = itemsInsideEachShop["Quantity"] as! Int
+                    let price: Int = itemsInsideEachShop["Price"] as! Int
+                    let qty: Int  = itemsInsideEachShop["Quantity"] as! Int
                     total_price = total_price + (price * qty)
                     
                     print("- \(itemsInsideEachShop["Name"] ?? "") x \(itemsInsideEachShop["Quantity"] ?? "")")
@@ -80,7 +88,7 @@ repeat{
             }
             
             
-            //print cart
+            //print cart choices
             print("""
             Press [B] to go back
             Press [P] to pay / checkout
@@ -88,27 +96,33 @@ repeat{
             """)
             
             var userPay = " "
-               
+            
+            //logic for checker either back or pay
             while(true){
                 userPay = readLine()?.uppercased() ?? " "
-                //for shopping cart or quit
+                //for shopping cart or back
                 if userPay == "B" || userPay == "P" {
                     break
                 }
                 print("That is not a valid response! Please Try Again.")
             }
+            //     logic end
+            
+            //switch for buy or pay
             if(userPay == "B"){
                 break
             } else{
+                //call pay function
                 pay(total_price: total_price)
                 break
             }
         }
             	
     default:
+        //else print each menu of restaurant
         printFoods(shops: shops, userInput: Int(userInput)!)
     }
     
 } while(userInput != "Q")
-
+//conditional for Q for quit
 
--- 0706012110037-Hagen Kwik-AFL1/restaurant.swift
+++ 0706012110037-Hagen Kwik-AFL1/restaurant.swift
@@ -7,14 +7,15 @@
 
 import Foundation
 
-
+// print menu or each restaurant func
 func printFoods(shops: KeyValuePairs<String, KeyValuePairs<String, Int> >, userInput: Int){
     var userInputForFood = " "
     
-    print("Hi, welcome back to \(shops[userInput].key)!")
-    print("What would you like to order?")
-    
-    while true{
+    //condition for if user want to go back to main screen
+    repeat {
+        //print food
+        print("Hi, welcome back to \(shops[userInput].key)!")
+        print("What would you like to order?")
         var num = 1
         for MenuItems in shops[userInput].value {
             print("[\(num)] \(MenuItems.key)")
@@ -26,33 +27,32 @@ func printFoods(shops: KeyValuePairs<String, KeyValuePairs<String, Int> >, userI
     Your menu choice?
     """)
         
-    //CHOOSINF CHECKER LOGIC
-    while true{
-        userInputForFood = readLine()?.uppercased() ?? " "
-        //for shopping cart or quit
-        if userInputForFood == "B" {
-            break
-        }
-        //check if number
-        if Int(userInputForFood) ?? -1 < shops[userInput].value.count+1 && Int(userInputForFood) ?? -1 > 0{
-            userInputForFood = String(Int(userInputForFood)!-1)
-            break
-        }
-        
-        print("That is not a valid response! Please Try Again.")
-    }
-    
-    switch(userInputForFood){
-        case "B":
-            break
-        default:
-        
-            print(shops[userInput].value[Int(userInputForFood)!].key + " @ \( shops[userInput].value[Int(userInputForFood)!].value )")
-        
-            print("How many \(shops[userInput].value[Int(userInputForFood)!].key) do you want to buy ?")
+        //CHOOSINg CHECKER LOGIC
+        while true{
             
+            userInputForFood = readLine()?.uppercased() ?? " "
+            //for shopping cart or quit
+            if userInputForFood == "B" {
+                break
+            }
+            //check if number
+            if Int(userInputForFood) ?? -1 < shops[userInput].value.count+1 && Int(userInputForFood) ?? -1 > 0{
+                userInputForFood = String(Int(userInputForFood)!-1)
+                break
+            }
+            
+            print("That is not a valid response! Please Try Again.")
+        }
+//        choosing logic end
+        
+        //check if input is back or not
+        if (userInputForFood != "B"){
+            //print food, price and how many you want to buy
+            print(shops[userInput].value[Int(userInputForFood)!].key + " @ \( shops[userInput].value[Int(userInputForFood)!].value )")
+            print("How many \(shops[userInput].value[Int(userInputForFood)!].key) do you want to buy ?")
             var userInputForBuying = " "
-
+            
+            //input checker for number or not
             while true {
                 userInputForBuying = readLine()?.uppercased() ?? " "
                 if Int(userInputForBuying) ?? -1 > 0{
@@ -60,38 +60,46 @@ func printFoods(shops: KeyValuePairs<String, KeyValuePairs<String, Int> >, userI
                 }
                 print("Please Enter a valid number")
             }
-        
-    addFoodtoCart(name: shops[userInput].value[Int(userInputForFood)!].key, price: shops[userInput].value[Int(userInputForFood)!].value , quantity: Int(userInputForBuying)!, StoreName: shops[userInput].key )
-
+            
+            //call function to add to shopping cart
+            addFoodtoCart(name: shops[userInput].value[Int(userInputForFood)!].key, price: shops[userInput].value[Int(userInputForFood)!].value , quantity: Int(userInputForBuying)!, StoreName: shops[userInput].key )
+            
             print("Thank You for Ordering")
+            print("")
         }
-    
-    break
-    }
+        
+    } while (userInputForFood != "B")
+    //checker if input is b or not
 }
 
+// function add to call
 func addFoodtoCart(name: String, price: Int, quantity: Int, StoreName: String) {
-    var temp: [String: Any] = ["Name": name, "Price": price, "Quantity": quantity]
+    //intitialize dictionary/keyvalue pair to be put in shopping cart
+    let temp: [String: Any] = ["Name": name, "Price": price, "Quantity": quantity]
     
     //check if restaurant is present
     if carts[StoreName] != nil {
-        //check so no double food if no double input new
+        //check so no double food, call func
         if !checkdoublePay(StoreName: StoreName, name: name, quantity: quantity) {
+            //if no double then push
             carts[StoreName]!.append(temp)
         }
     } else {
-        var TempArray = [Dictionary<String, Any>] ()
+        //if no food at all from the restaurant, create array first to be put in the shopping cart then input the food
+        //reason to create array first is because the shopping cart is an empty dictionary
+        let TempArray = [Dictionary<String, Any>] ()
 
         carts[StoreName] = TempArray
         carts[StoreName]!.append(temp)
 
     }
-    print(carts)
 }
 
+//func to check if food is present in shopping cart
 func checkdoublePay(StoreName: String, name: String, quantity: Int) -> Bool {
     var numTEMP = 0
     
+    //logic for checking if food is present in shopping cart
     for items in carts[StoreName]! {
         if items["Name"] as! String == name {
             carts[StoreName]![numTEMP]["Quantity"] = items["Quantity"] as! Int + quantity
@@ -102,17 +110,15 @@ func checkdoublePay(StoreName: String, name: String, quantity: Int) -> Bool {
     
     return false
 }
-    
-//    carts[StoreName]!.append(temp)
-//}
-
-
+//function to pay
 func pay(total_price: Int){
+    
     print("Your total order: \(total_price)")
     print("Enter the amount of your money: ")
     
     var your_money = " "
     
+    //input checker logic
     while true {
         your_money = readLine() ?? " "
         //bigger than 0 and is bigger than pice
@@ -122,6 +128,7 @@ func pay(total_price: Int){
             print("Payment not enough")
         }
         
+        //chcek if number
         if Int(your_money) ?? -1 == -1 || Int(your_money) ?? -1 == 0{
             print("Please enter a valid amount")
         }
@@ -130,11 +137,13 @@ func pay(total_price: Int){
     print("You pay \(your_money)  Change:\(Int(your_money)! - total_price)")
     print("Enjoy your meals!")
     
-//    carts["Tuku-Tuku"].removeAll()
+    //clear shopping cart after
+    carts = [:]
     
     var returnBool = "A"
-
+    
     while true{
+//        go back
         print("Press [return] to go back to main screen")
         returnBool = readLine() ?? "A"
         if returnBool == "" {
